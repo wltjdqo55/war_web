95ae2880641504eb1d4df3e7dbdf11b605dee993
지성배 <seongbae@eu-cast.com>
2024-08-29 11:27:30 +0900

    first upload

---
+++ src/main/java/kingdom/warPrj/WarPrjApplication.java
@@ -0,0 +1,13 @@
+package kingdom.warPrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kingdom/warPrj/WarPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package kingdom.warPrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
