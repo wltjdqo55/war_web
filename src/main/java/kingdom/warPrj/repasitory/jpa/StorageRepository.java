6b145a4d2dc9e488bf6d91d96b62ef79a563ca69
지성배 <seongbae@eu-cast.com>
2024-09-05 13:21:30 +0900

    240905
    1. 수장고 등록 구현
    2. 수장고 목록 구현
    3. 수장고 수정 구현
    4. 수장고 삭제 죽현
    5. 검색기능 구현

---
+++ src/main/java/kingdom/warPrj/repasitory/jpa/StorageRepository.java
@@ -0,0 +1,13 @@
+package kingdom.warPrj.repasitory.jpa;
+
+import kingdom.warPrj.entity.entity.SpeciesEntity;
+import kingdom.warPrj.entity.entity.StorageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StorageRepository extends JpaRepository<StorageEntity, Long> {
+  Optional<StorageEntity> findOneByItem(String itemName);
+}
