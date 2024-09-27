3f33be0333d1074bccab69df4e3b905c96d65f4c
지성배 <seongbae@eu-cast.com>
2024-09-27 10:29:11 +0900

    240927
    1. 인터셉터 코드 수정

--- src/main/java/kingdom/warPrj/config/WebConfig.java
+++ src/main/java/kingdom/warPrj/config/WebConfig.java
@@ -14,12 +14,16 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
+
+    String[] interceptorPath = new String[]{"/account/admin/**", "/account/analysis", "/account/battle"
+        , "/account/soldier/add", "/account/soldier/edit/**", "/account/skill/add", "/account/skill/edit/**"
+        , "/account/species/add", "/account/species/edit/**", "/account/storage/add", "/account/storage/edit/**"
+        , "/account/legion/add", "/account/legion/edit/**", "/account/general/add", "/account/general/edit/*"};
+
+    String[] excludePath = new String[]{"/account/login", "/account/error", "/account/loginCheck"};
+
     registry.addInterceptor(sessionCheckInterceptor)
-        .addPathPatterns("/account/admin/**", "/account/analysis", "/account/battle"
-            , "/account/soldier/add", "/account/soldier/edit/**", "/account/skill/add", "/account/skill/edit/**"
-            , "/account/species/add", "/account/species/edit/**", "/account/storage/add", "/account/storage/edit/**"
-            , "/account/legion/add", "/account/legion/edit/**", "/account/general/add", "/account/general/edit/*"
-        ) // 필요한 경로에만 인터셉터 적용
-        .excludePathPatterns("/account/login", "/account/error", "/account/loginCheck"); // 로그인 및 에러 페이지는 제외
+        .addPathPatterns(interceptorPath) // 필요한 경로에만 인터셉터 적용
+        .excludePathPatterns(excludePath); // 로그인 및 에러 페이지는 제외
   }
 }
